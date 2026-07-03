11d3a8b55fbbcc38fe62c28e5694b02481660e02
anshvarshney2416-jpg <ansh.varshney2416@gmail.com>
2026-07-03 15:38:36 +0530

    Add gunicorn for Render

--- app.py
+++ app.py
@@ -182,6 +182,8 @@ def clear():
     )
 
 
-if __name__ == "__main__":
+import os
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
